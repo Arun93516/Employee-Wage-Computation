66552318f689211ac36c2b0077b2cae849934bab
Arun93516 <arunkumar93516@gmail.com>
2022-09-04 13:03:29 +0530

    Added: Employee Is Present or Absent Today

--- src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
+++ src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
@@ -1,10 +1,17 @@
 package moc.noitatuomoc.eeyolpme;
 
+import java.util.Random;
+
 public class EmployeeWagae {
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to LineComparison ComputationProgram on Master Branch");
-
+		System.out.println("Welcome to LineComparison ComputationProgram on Master Branch");
+		Random random = new Random();
+		int randomNumber = random.nextInt(2);
+		if (randomNumber == 1)
+			System.out.println("Employee is present Today ");
+		else
+			System.out.println("Employee is Absent Today ");
 	}
 
 }
