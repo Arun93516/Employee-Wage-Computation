96e8d24f80a8cee92e1cdfc90c78bb766deb7692
Arun93516 <arunkumar93516@gmail.com>
2022-09-04 15:30:07 +0530

    Added: Employee is Working Full time or Partime Based to Gives The Salay

--- src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
+++ src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
@@ -6,22 +6,23 @@ public class EmployeeWagae {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to LineComparison ComputationProgram on Master Branch");
-		
-		int PerhourWage=20 ;
-	    int FullDay=8;
-	     int Salary=0;
-		
-		
-		
+
+		int PerhourWage = 20;
+		int FullDay = 8;
+		int Salary = 0;
+		int HalfDay = 4;
+
 		Random random = new Random();
-		int randomNumber = random.nextInt(2);
-		if (randomNumber == 1){
-			System.out.println("Employee is present Today ");
-		     Salary= PerhourWage *FullDay;
-		}
-		     else
+		int randomNumber = random.nextInt(3);
+		if (randomNumber == 1) {
+			System.out.println("Employee is present Full Day  ");
+			Salary = PerhourWage * FullDay;
+		} else if (randomNumber == 2) {
+			System.out.println("Employee is Working Half Day only ");
+			Salary = PerhourWage * HalfDay;
+		} else
 			System.out.println("Employee is Absent Today ");
-		System.out.println("Employee Salary is :"+Salary);
+		System.out.println("Employee Salary is :" + Salary);
 	}
 
 }
