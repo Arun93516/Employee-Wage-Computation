7eeb82d78c60700e9228e7f7d39edf5d9cfa3dcd
Arun93516 <arunkumar93516@gmail.com>
2022-09-05 19:40:20 +0530

    Added : Monthly Salary  Wage Calculation depends on working hours

--- src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
+++ src/moc/noitatuomoc/eeyolpme/EmployeeWagae.java
@@ -5,39 +5,43 @@ import java.util.Random;
 public class EmployeeWagae {
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to LineComparison ComputationProgram on Master Branch");
 
 		int PerhourWage = 20;
 		int FullDay = 8;
 		int Salary = 0;
 		int HalfDay = 4;
-        int WorkingDayPerMonth=20;
-        int monthlySalary=0;
-        
-        Random random = new Random();
-	   int randomNumber = random.nextInt(3);
-		
-	   
-	   for ( int i=1 ; i<= WorkingDayPerMonth ; i++)
-	   {
-	   switch (randomNumber) {
-		case 1:
-			System.out.println("Employee is present Full Day  ");
-			Salary = PerhourWage * FullDay;
-			break;
-		case 2:
-			System.out.println("Employee is Working Half Day only ");
-			Salary = PerhourWage * HalfDay;
-			break;
-		case 0:
-			System.out.println("Employee is Absent Today ");
-		}
-	   
-	   monthlySalary =monthlySalary+Salary;
-	  
-	}
-	   System.out.println("Employee MonthlySalary is :" + monthlySalary);
-	}	
-	}
-	
+		int WorkingDayPerMonth = 20;
+		int monthlySalary = 0;
+		int WorkingHourPerMonth = 0;
+		int Days = 1;
 
+		Random random = new Random();
+		int randomNumber = random.nextInt(3);
+
+		while (Days != 20 && WorkingHourPerMonth != 100) {
+
+			switch (randomNumber) {
+			case 1:
+				System.out.println("Employee is present Full Day  ");
+				Salary = PerhourWage * FullDay;
+				WorkingHourPerMonth = WorkingHourPerMonth + FullDay;
+				break;
+			case 2:
+				System.out.println("Employee is Working Half Day only ");
+				WorkingHourPerMonth = WorkingHourPerMonth + HalfDay;
+				Salary = PerhourWage * HalfDay;
+				break;
+			case 0:
+				System.out.println("Employee is Absent Today ");
+			}
+
+			monthlySalary = monthlySalary + Salary;
+			System.out.println("Day " + Days + " : " + Salary);
+			Days++;
+		}
+
+		System.out.println("Employee Monthly Salary is : " + monthlySalary);
+	}
+}
